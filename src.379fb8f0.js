a3e0d68621cdc3f97898a157c2609696fabd0727
voprim <76738605+voprim@users.noreply.github.com>
2021-07-12 10:52:23 +0000

    Deploying to gh-pages from @ voprim/goit-js-hw-10-food-service@d81f1a128cfad099dab69714794199077a8bcf81 🚀

--- src.379fb8f0.js
+++ src.379fb8f0.js
@@ -1,7 +1,7 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
 },{}],"qwQt":[function(require,module,exports) {
-const e={LIGHT:"light-theme",DARK:"dark-theme"},t=document.querySelector("#theme-switch-toggle");function c(e){e.currentTarget.checked?o():n()}function o(){document.body.classList.add(e.DARK),document.body.classList.remove(e.LIGHT),localStorage.setItem("theme",e.DARK),t.checked=!0}function n(){document.body.classList.add(e.LIGHT),document.body.classList.remove(e.DARK),localStorage.setItem("theme",e.LIGHT),t.checked=!1}function d(){const t=localStorage.getItem("theme");t!==e.LIGHT&&null!==t?t!==e.DARK||o():n()}t.addEventListener("change",c),d();
+const e={LIGHT:"light-theme",DARK:"dark-theme"},t=document.querySelector("#theme-switch-toggle");function c(t){t.currentTarget.checked?o(e.DARK,e.LIGHT,!0):o(e.LIGHT,e.DARK,!1)}function o(e,c,o){document.body.classList.add(e),document.body.classList.remove(c),localStorage.setItem("theme",e),t.checked=o}function n(){const t=localStorage.getItem("theme");t!==e.LIGHT&&null!==t?t!==e.DARK||o(e.DARK,e.LIGHT,!0):o(e.LIGHT,e.DARK,!1)}t.addEventListener("change",c),n();
 },{}],"pAws":[function(require,module,exports) {
 var define;
 var global = arguments[3];
@@ -15,4 +15,4 @@ module.exports=[{id:"XWaQXcbk0",name:"Картофель, запеченный �
 },{"../templates/cards.hbs":"fpKt","../menu.json":"mA8c"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/theme"),require("./js/create-cards");
 },{"./sass/main.scss":"clu1","./js/theme":"qwQt","./js/create-cards":"kCAY"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.5244f4e1.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.379fb8f0.js.map
